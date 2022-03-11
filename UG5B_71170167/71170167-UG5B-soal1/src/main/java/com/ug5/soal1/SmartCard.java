ed5a52f47f532d3a772792598bcd2ab85d0312b1
Abraham Nendissa <54307026+abrahamnendissa@users.noreply.github.com>
2022-03-11 10:05:34 +0700

    Add files via upload

---
+++ UG5B_71170167/71170167-UG5B-soal1/src/main/java/com/ug5/soal1/SmartCard.java
@@ -0,0 +1,26 @@
+package com.ug5.soal1;
+
+public class SmartCard {
+    private String nama;
+    private long saldo;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public long getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(long saldo) {
+        this.saldo = saldo;
+    }
+
+    public void topUp(Voucher vcr){
+
+    }
+}
---
+++ UG5B_71170167/71170167-UG5B-soal2/src/main/java/com/ug5/soal2/Buku.java
@@ -0,0 +1,38 @@
+package com.ug5.soal2;
+
+public class Buku {
+    private String kode;
+    private int nectNum;
+    private String judul;
+    private String penulis;
+    private long harga;
+    private int stok;
+
+    public Buku(String argJudul, String argPenulis, long argHarga, int argStok) {
+
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public int getNectNum() {
+        return nectNum;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public String getPenulis() {
+        return penulis;
+    }
+
+    public long getHarga() {
+        return harga;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+}
